5df534567d1a7183029836771c72f0af12e4083f
Maggie-sun1225 <72954125+Maggie-sun1225@users.noreply.github.com>
2022-10-23 22:45:24 -0700

    Update md5.c

--- src/md5sum/md5.c
+++ src/md5sum/md5.c
@@ -12,7 +12,9 @@
 #include <string.h>
 #include <stdint.h>
 
-#include "support.h"
+//#include "support.h"
+
+#define CPU_MHZ 1
 
 #define LOCAL_SCALE_FACTOR 51
 
@@ -30,6 +32,69 @@ static char heap[HEAP_SIZE];
 
 // leftrotate function definition
 #define LEFTROTATE(x, c) (((x) << (c)) | ((x) >> (32 - (c))))
+
+/* BEEBS version of calloc.
+   Implement as wrapper for malloc */
+
+void *
+calloc_beebs (size_t nmemb, size_t size)
+{
+  void *new_ptr = malloc_beebs (nmemb * size);
+
+  /* Calloc is defined to zero the memory. OK to use a function here, because
+     it will be handled specially by the compiler anyway. */
+
+  if (NULL != new_ptr)
+    memset (new_ptr, 0, nmemb * size);
+
+  return new_ptr;
+}
+
+
+/* BEEBS version of malloc.
+   This is primarily to reduce library and OS dependencies. Malloc is
+   generally not used in embedded code, or if it is, only in well defined
+   contexts to pre-allocate a fixed amount of memory. So this simplistic
+   implementation is just fine.
+   Note in particular the assumption that memory will never be freed! */
+
+void *
+malloc_beebs (size_t size)
+{
+  void *new_ptr = heap_ptr;
+
+  heap_requested += size;
+
+  if (((void *) ((char *) heap_ptr + size) > heap_end) || (0 == size))
+    return NULL;
+  else
+    {
+      heap_ptr = (void *) ((char *) heap_ptr + size);
+      return new_ptr;
+    }
+}
+
+
+/* BEEBS version of free.
+   For our simplified version of memory handling, free can just do nothing. */
+
+void
+free_beebs (void *ptr __attribute__ ((unused)))
+{
+}
+
+/* Initialize the BEEBS heap pointers. Note that the actual memory block is
+   in the caller code. */
+
+void
+init_heap_beebs (void *heap, size_t heap_size)
+{
+  heap_ptr = (void *) heap;
+  heap_end = (void *) ((char *) heap_ptr + heap_size);
+  heap_requested = 0;
+}
+
+
 
 // These vars will contain the hash
 static uint32_t h0, h1, h2, h3;
@@ -240,4 +305,13 @@ verify_benchmark (int r)
 {
   // This isn't a proper check...
   return r == RESULT;
+}
+
+int main(){
+
+  __attribute__ ((noinline));
+  
+
+  return 0;
+
 }
